1fc6b57c57a737917befc09ad8f0607d9df0f325
Sergii Puzyrov <svpuzyrev@gmail.com>
2026-03-11 15:01:22 +0200

    [FIX] Fixed manual typing of host port for UDP app

--- projects/udp/senders/simple_udp_sender/src/simple_udp_sender.cpp
+++ projects/udp/senders/simple_udp_sender/src/simple_udp_sender.cpp
@@ -3,49 +3,58 @@
 void exit_handler();
 
 SOCKET client_socket;
+
 int main(int argc, char* argv[])
 {
-	atexit(common_exit_handler);
-	atexit(exit_handler);
+    atexit(common_exit_handler);
+    atexit(exit_handler);
 
-	short port;
-	char host[128] = "";
-	bool parse_cmd_result = parse_cmd(argc, argv, host, &port);
+    short port;
+    char host[128] = "";
+    bool parse_cmd_result = parse_cmd(argc, argv, host, &port);
 
-	if (!parse_cmd_result || !host || !strlen(host))
-	{
-		printf("Invalid host or port. Usage %s -h host -p port\n", argv[0]);
-		return -1;
-	}
+    if (!parse_cmd_result || !strlen(host))
+    {
+        printf("Enter end-point in format -h <host> -p <port>:");
+
+        if (scanf("-h %s -p %d", host, &port) < 2)
+        {
+            printf("Error input\n");
+            return -1;
+        }
+
+        getc(stdin);
+    }
 
     common_init_handler();
 
-	client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (client_socket <= 0) {
-		error_msg("Can't create socket");
-		return -1;
-	}
+    client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (client_socket <= 0)
+    {
+        error_msg("Can't create socket");
+        return -1;
+    }
 
-	struct sockaddr_in server_addr;
-	init_inet_address(&server_addr, host, port);
+    struct sockaddr_in server_addr;
+    init_inet_address(&server_addr, host, port);
 
-	
-	char msg[256] = "";
-	printf("%s", "Enter msg:");
-	//fgets(msg, sizeof(msg), stdin);
-	scanf("%[^\n]s", msg);
-	int sc = sendto(client_socket, msg, sizeof(msg), 0, (sockaddr*)&server_addr, sizeof(server_addr));
-	if (sc <= 0) {
-		char err_msg[128] = "";
-		sprintf(err_msg, "Can't send data to the %s:%d", host, port);
-		error_msg(err_msg);
-		return -1;
-	}
 
-	return 0;
+    char msg[256] = "";
+    printf("%s", "Enter msg:");
+    scanf("%[^\n]s", msg);
+    int sc = sendto(client_socket, msg, sizeof(msg), 0, (sockaddr*)&server_addr, sizeof(server_addr));
+    if (sc <= 0)
+    {
+        char err_msg[128] = "";
+        sprintf(err_msg, "Can't send data to the %s:%d", host, port);
+        error_msg(err_msg);
+        return -1;
+    }
+
+    return 0;
 }
 
 void exit_handler()
 {
-	close_socket(client_socket);
+    close_socket(client_socket);
 }
